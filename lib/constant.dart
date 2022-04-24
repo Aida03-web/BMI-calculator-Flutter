899d3a74e9a6ef1c62416eabee690faa72499f28
Aida <agaziz157@gmail.com>
2022-04-24 18:02:32 +0600

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+const inactiveCardColour = Color(0xFF111328);
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB555);
+const bottomContainerHeight = 80.0;
+
+const labelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const numberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const LargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const resultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const BMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const BodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
